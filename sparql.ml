6bb53df41d81a7d67ba47b26b59985134cbd2099
Sebastien Ferre <ferre@irisa.fr>
2017-06-09 15:34:25 +0200

    README, COPYING and copyright notice in each source file

--- sparql.ml
+++ sparql.ml
@@ -1,3 +1,21 @@
+(*
+  Copyright 2013-2017 Sébastien Ferré, IRISA, Université de Rennes 1
+
+  This file is part of Sparklis.
+
+  Sparklis is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  Sparklis is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with Sparklis.  If not, see <http://www.gnu.org/licenses/>.
+*)
 
 module S : (* private use of strings to represent SPARQL strings *)
 sig
